07ad8b542335576948dd3ba594e4ef0a697f9bce
medam charan <charanm5049@gmail.com>
2025-04-23 17:27:11 +0530

    updated

--- BackendApp-BlogApp-NormalApp/src/main/java/com/example/blogapp/controllers/UserController.java
+++ BackendApp-BlogApp-NormalApp/src/main/java/com/example/blogapp/controllers/UserController.java
@@ -1,20 +1,27 @@
 package com.example.blogapp.controllers;
 
-import com.example.blogapp.models.User;
-import com.example.blogapp.services.UserService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.blogapp.models.User;
+import com.example.blogapp.services.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
-    @CrossOrigin(origins = "http://localhost:3000")
+   
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
